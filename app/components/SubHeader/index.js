1bfa30bf53c16e2fd24acfb77f91ce9f6ded36ee
Paul Laux <lauxpaul@protonmail.com>
2018-07-30 14:43:06 +0300

    added exchange list

--- app/components/SubHeader/index.js
+++ app/components/SubHeader/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Row, Col } from 'antd';
-import { exchanges } from 'utils/constants';
+// import { exchanges } from 'utils/constants';
 
 const RowS = styled(Row)`
   div{
@@ -19,10 +19,12 @@ const RowS = styled(Row)`
   }
 `;
 
+const url = 'https://monetarycoin.org/monetarycoin_exchange_list';
+
 function SubHeader(props) {
   const { tokenSymbol } = props;
   const token = tokenSymbol || 'MERO';
-  const { name, url } = exchanges[token][0];
+  // const { name, url } = exchanges[token][0];
   return (
     <RowS type="flex" >
       <Col xs={{ span: 20, offset: 1 }}>
@@ -31,9 +33,12 @@ function SubHeader(props) {
           <a target="_blank" rel="noopener" href="https://monetarycoin.io">
             {' MonetaryCoin Distribution '}
           </a>
-          or the
-          <a target="_blank" rel="noopener" href={url}>
+          or see the
+          {/* <a target="_blank" rel="noopener" href={url}>
             {` ${name}`} exchange
+          </a> */}
+          <a target="_blank" rel="noopener" href={url}>
+            {` ${token}`} Exchange List
           </a>
         </span>
       </Col>
